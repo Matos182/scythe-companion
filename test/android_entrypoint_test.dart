b5753da66a294c6508727d41d72dbd720a11b72b
Matos182 <fmlmatos@gmail.com>
2026-07-21 14:47:14 +0000

    no-mistakes(review): Harden Android launcher activity regression validation

--- test/android_entrypoint_test.dart
+++ test/android_entrypoint_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:xml/xml.dart';
 
 void main() {
   test('Android launcher activity matches the application namespace', () {
@@ -15,16 +16,44 @@ void main() {
     final applicationId = RegExp(
       r'''applicationId\s+["']([^"']+)["']''',
     ).firstMatch(gradle)!.group(1)!;
-    final activityName = RegExp(
-      r'''<activity\b[^>]*android:name=["']([^"']+)["']''',
-      dotAll: true,
-    ).firstMatch(manifest)!.group(1)!;
+    const androidNamespace = 'http://schemas.android.com/apk/res/android';
+    final document = XmlDocument.parse(manifest);
+    final launcherActivities = document
+        .findAllElements('activity')
+        .where(
+          (activity) => activity.findElements('intent-filter').any(
+            (filter) =>
+                filter.findElements('action').any(
+                  (action) =>
+                      action.getAttribute(
+                        'name',
+                        namespace: androidNamespace,
+                      ) ==
+                      'android.intent.action.MAIN',
+                ) &&
+                filter.findElements('category').any(
+                  (category) =>
+                      category.getAttribute(
+                        'name',
+                        namespace: androidNamespace,
+                      ) ==
+                      'android.intent.category.LAUNCHER',
+                ),
+          ),
+        )
+        .toList();
 
     expect(applicationId, 'com.matos.scythe_companion');
     expect(namespace, applicationId);
-    expect(activityName, startsWith('.'));
+    expect(launcherActivities, hasLength(1));
 
-    final className = activityName.substring(1);
+    final activityName = launcherActivities.single.getAttribute(
+      'name',
+      namespace: androidNamespace,
+    );
+    expect(activityName, '.MainActivity');
+
+    final className = activityName!.substring(1);
     final source = File(
       'android/app/src/main/kotlin/${namespace.replaceAll('.', '/')}/$className.kt',
     );
@@ -36,7 +65,12 @@ void main() {
     );
     expect(
       source.readAsStringSync(),
-      contains('package $namespace'),
+      matches(
+        RegExp(
+          '^[ \\t]*package[ \\t]+${RegExp.escape(namespace)}[ \\t]*;?[ \\t]*\$',
+          multiLine: true,
+        ),
+      ),
       reason: 'The launcher activity package must match the Android namespace.',
     );
   });
